521227a4974bb7cb95b5307f08da29328e6f41e3
Chethan-Raj <72667243+Chethan-Raj@users.noreply.github.com>
2020-10-10 23:28:14 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Employee
+{
+    public static void display()
+    {
+        System.out.println("The name is Saurab");
+        System.out.println("The age is 23");
+        System.out.println("The City is Chennai");
+
+    }
+    public static void main (String[] args){
+        display();
+
+        display();
+    }
+}
+
+
